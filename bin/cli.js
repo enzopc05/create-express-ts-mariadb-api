1a3dab12081606803cd6388b702ca34e811df6e6
Roméo <giorgio.romeo95@gmail.com>
2023-12-05 23:40:25 +0100

    Initial commit - V1.0.0

---
+++ bin/cli.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const {execSync} = require('child_process');
+
+const runCommand = command=>{
+    try {
+        execSync(`${command}`, {stdio: 'inherit'});
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+    return true; 
+}
+
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/Romeo-Giorgio/create-ts-mariadb-api.git ${repoName}`;
+const installDepsCommand = `cd ${repoName} && npm install`;
+
+console.log(`Cloning rerpository with name ${repoName}`);
+const checkedOut = runCommand(gitCheckoutCommand);
+if(!checkedOut) process.exit(-1);
+
+console.log(`Installing dependencies for ${repoName}`);
+const installedDeps = runCommand(installDepsCommand);
+if(!installedDeps) process.exit(-1);
+
+console.log("Congratulations ! You are ready. Follow the following command to start");
+console.log(`cd ${repoName} && npm run dev`);
